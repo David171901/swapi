bf51b638f27226faff2573abe50b03fd94832fd2
david.pino <david@ip-172-31-89-46.ec2.internal>
2023-06-01 17:53:06 +0000

    build: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://David171901.github.io/swapi/',
   test: {
     environment: 'jsdom',
     globals: true,

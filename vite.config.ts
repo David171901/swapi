db8b39ea079d584aef9cebf49813387f853777a5
david.pino <david@ip-172-31-89-46.ec2.internal>
2023-06-01 18:01:28 +0000

    build: base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/swapi/',
+  base: '/',
   test: {
     environment: 'jsdom',
     globals: true,
